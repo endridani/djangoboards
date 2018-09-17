82ab5e11441fcffbe753a6811637bd4fa5bf366b
Endri Dani <endri.dani@gmail.com>
2018-09-17 19:03:16 +0200

    Added views and their respective templates for signup, login, logout, password reset and password change.

--- simpleboards/urls.py
+++ simpleboards/urls.py
@@ -14,13 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from board import views
 from accounts import views as account_views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('reset/', include('accounts.urls')),
+    path('settings/password/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+         name='password_change'),
+    path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+         name='password_change_done'),
     path('signup/', account_views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
