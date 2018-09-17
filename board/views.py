91f20dd296ee479685d5e66da57e53756de26ec7
Endri Dani <endri.dani@gmail.com>
2018-09-17 15:55:49 +0200

    Improved views.py and templates for 'board' app. Added 'accounts' app.

--- board/views.py
+++ board/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from board.models import Board
+from django.shortcuts import render, redirect, get_object_or_404
+from board.models import Board, Topic, Post, User
+from board.forms import NewTopicForm
 
 
 def home(request):
@@ -10,3 +11,24 @@ def home(request):
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
--- simpleboards/settings.py
+++ simpleboards/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'board'
+    'widget_tweaks',
+    'board',
+    'accounts',
 ]
 
 MIDDLEWARE = [
